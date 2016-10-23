01c31fd73b0eca66284cdaa932ddcf7e53c0ead0
Иванов Данила Алексеевич <daniel.a.ivanov@yandex.ru>
2016-10-23 09:13:17 +0400

    Изменены логотипы приложения

---
+++ app/src/main/java/ru/dtechnologies/schedule_train/ChooseActivity.java
@@ -0,0 +1,327 @@
+package ru.dtechnologies.schedule_train;
+
+import android.app.Fragment;
+import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.ArrayMap;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import ru.dtechnologies.schedule_train.parser.JSONParser;
+
+
+/**
+ * Created by Admin on 20.10.2016.
+ */
+
+public class ChooseActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener {
+
+    ListView lvSchedule;
+    TextView tvCountry, tvRegion, tvDiscrict, tvSity, tvName;
+    Button btnCancle, btnChoose;
+    RelativeLayout layoutInfo;
+
+    private static final String LOG_TAG = "myLogs";
+
+    private static final String TAG_DATA = "extra_data";
+
+    private static final String TAG_TABLE_FROM = "citiesFrom";
+    private static final String TAG_TABLE_TO = "citiesTo";
+
+    private static final String TAG_COUNTRY = "countryTitle";
+    private static final String TAG_REGION = "regionTitle";
+    private static final String TAG_DISTRICT = "districtTitle";
+    private static final String TAG_SITY = "cityTitle";
+    private static final String TAG_ID = "stationId";
+    private static final String TAG_LOCATION_1 = "location_1";
+    private static final String TAG_LOCATION_2 = "location_2";
+
+    private static final String TAG_TABLE_STATIONS = "stations";
+    private static final String TAG_NAME_STATION = "stationTitle";
+
+    private SearchView mSearchView;
+    private MenuItem searchMenuItem;
+
+
+
+    // url получения списка всех
+    private static String url_get_schedule = "https://raw.githubusercontent.com/tutu-ru/hire_android_test/master/allStations.json";
+
+    private ProgressDialog pDialog;
+    // Создаем JSON парсер
+    JSONParser jParser = new JSONParser();
+
+    ArrayList<HashMap<String, String>> listStation = new ArrayList<HashMap<String, String>>();
+    SimpleAdapter adapter;
+    String scheduleTable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.d(LOG_TAG, "query = "+query);
+
+        } else if (Intent.ACTION_VIEW.equals(intent.getAction())) {
+            String uri = intent.getDataString();
+            Log.d(LOG_TAG, "uri = "+uri);
+        }
+
+        lvSchedule = (ListView) findViewById(R.id.lvSchedule);
+
+        layoutInfo = (RelativeLayout) findViewById(R.id.layout_details_item);
+        tvName = (TextView) findViewById(R.id.tvNameStation);
+        tvCountry = (TextView) findViewById(R.id.tvCountry_toast);
+        tvRegion = (TextView) findViewById(R.id.tvRegion_toast);
+        tvDiscrict = (TextView) findViewById(R.id.tvDistrict_toast);
+        tvSity = (TextView) findViewById(R.id.tvSity_toast);
+
+        btnChoose = (Button) findViewById(R.id.btnChoose_toast);
+        btnCancle = (Button) findViewById(R.id.btnCancle_toast);
+
+        btnChoose.setOnClickListener(this);
+        btnCancle.setOnClickListener(this);
+
+        lvSchedule.setOnItemClickListener(this);
+
+        scheduleTable = getIntent().getStringExtra(TAG_DATA);
+        new LoadSchedule().execute(scheduleTable);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()){
+            case R.id.btnChoose_toast:
+                intent = new Intent(ChooseActivity.this, MainActivity.class);
+                intent.putExtra(TAG_NAME_STATION, tvName.getText());
+                if (tvRegion.length() == 0){
+                    intent.putExtra(TAG_LOCATION_1, tvCountry.getText());
+                }else {
+                    intent.putExtra(TAG_LOCATION_1, tvCountry.getText() + ", " + tvRegion.getText());
+                }
+
+                if (tvDiscrict.length() == 0){
+                    intent.putExtra(TAG_LOCATION_2, tvSity.getText());
+                }else{
+                    intent.putExtra(TAG_LOCATION_2, tvSity.getText()+", "+tvDiscrict.getText());
+                }
+
+                intent.putExtra(TAG_DATA, scheduleTable);
+                Log.d(LOG_TAG,scheduleTable);
+                Log.d(LOG_TAG, tvCountry.getText()+", "+tvRegion.getText()+",\n"+
+                        tvDiscrict.getText()+", "+tvSity.getText());
+                startActivity(intent);
+                break;
+
+            case R.id.btnCancle_toast:
+                layoutInfo.setVisibility(View.INVISIBLE);
+                break;
+        }
+    }
+
+    class LoadSchedule extends AsyncTask<String, String, String> {
+
+        /**
+         * Перед началом фонового потока Show Progress Dialog
+         * */
+        String arg = "null";
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(ChooseActivity.this);
+            pDialog.setMessage("Загрузка рассписания. Подождите...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * Получаем все продукт из url
+         * */
+        protected String doInBackground(String... args) {
+               /* // Будет хранить параметры
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                // получаем JSON строк с URL
+                JSONObject json = jParser.makeHttpRequest(url_get_schedule, "GET", params);
+
+                try {
+                    JSONArray base_table = null;
+                    if (args[0] == "from"){
+                        // Получаем масив
+                        base_table = json.getJSONArray(TAG_TABLE_FROM);
+                    }else{
+                        // Получаем масив
+                        base_table = json.getJSONArray(TAG_TABLE_TO);
+                    }
+
+                    // перебор всех элементов
+                    for (int i = 0; i < base_table.length(); i++) {
+                        JSONObject c = base_table.getJSONObject(i);
+                        JSONArray table_stations = c.getJSONArray(TAG_TABLE_STATIONS);
+
+                        for (int j = 0; j < table_stations.length(); j++) {
+                            JSONObject p = table_stations.getJSONObject(j);
+                            HashMap<String, String> map = new HashMap<String, String>();
+
+                            Log.d(LOG_TAG, "Name = " + p.getString(TAG_NAME_STATION));
+                            //map.put(TAG_ID, p.getString(TAG_ID));
+                            map.put(TAG_NAME_STATION, p.getString(TAG_NAME_STATION));
+                            map.put(TAG_COUNTRY, p.getString(TAG_COUNTRY));
+                            map.put(TAG_REGION, p.getString(TAG_REGION));
+                            map.put(TAG_SITY, p.getString(TAG_SITY));
+                            map.put(TAG_DISTRICT, p.getString(TAG_DISTRICT));
+
+                            listStation.add(map);
+                        }
+                    }
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                */
+            HashMap<String, String> map = new HashMap<String, String>();
+
+            //map.put(TAG_ID, p.getString(TAG_ID));
+            map.put(TAG_NAME_STATION, "Werty");
+            map.put(TAG_COUNTRY, "Россия");
+            map.put(TAG_REGION, "Центральная россия");
+            map.put(TAG_SITY, "Савёлки");
+            map.put(TAG_DISTRICT, "Московская область");
+
+            listStation.add(map);
+
+            map = new HashMap<String, String>();
+
+            //map.put(TAG_ID, p.getString(TAG_ID));
+            map.put(TAG_NAME_STATION, "erty");
+            map.put(TAG_COUNTRY, "Россия");
+            map.put(TAG_REGION, "Центральная россия");
+            map.put(TAG_SITY, "Кимовск");
+            map.put(TAG_DISTRICT, "Тульская область");
+
+            listStation.add(map);
+
+            return null;
+        }
+
+        /**
+         * После завершения фоновой задачи закрываем прогрес диалог
+         * **/
+        protected void onPostExecute(String arg) {
+            // закрываем прогресс диалог после получение все продуктов
+            pDialog.dismiss();
+
+            Toast.makeText(ChooseActivity.this, "Рассписание обновлено!", Toast.LENGTH_SHORT).show();
+
+            adapter = new SimpleAdapter(ChooseActivity.this, listStation, R.layout.item,
+                    new String[] {TAG_NAME_STATION, TAG_COUNTRY, TAG_REGION, TAG_DISTRICT, TAG_SITY},
+                    new int[] {R.id.tvStation, R.id.tvCountry, R.id.tvRegion, R.id.tvDistrict, R.id.tvSity});
+            lvSchedule.setAdapter(adapter);
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        String str = String.valueOf((parent.getAdapter().getItem(position)));
+
+        layoutInfo.setVisibility(View.VISIBLE);
+        tvName.setText(search(TAG_NAME_STATION, str));
+        tvCountry.setText(search(TAG_COUNTRY, str));
+        tvRegion.setText(search(TAG_REGION, str));
+        tvDiscrict.setText(search(TAG_DISTRICT, str));
+        tvSity.setText(search(TAG_SITY, str));
+
+    }
+
+    public String search(String key, String string){
+        String value = null;
+        if (string.contains(key)){
+             value = string.substring(string.indexOf(key)+1+key.length(), string.length());
+            if (value.indexOf(',') != -1){
+                value = value.substring(0, value.indexOf(','));
+            }else{
+                value = value.substring(0, value.indexOf('}'));
+            }
+
+        }
+        return value;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_bar_choose, menu);
+
+        SearchView.OnQueryTextListener listener = new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // newText is text entered by user to SearchView
+                ChooseActivity.this.adapter.getFilter().filter(newText);
+                return false;
+            }
+        };
+
+        searchMenuItem = menu.findItem(R.id.search);
+        mSearchView = (SearchView) searchMenuItem.getActionView();
+        mSearchView.setOnQueryTextListener(listener);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
---
+++ app/src/main/java/ru/dtechnologies/schedule_train/MainActivity.java
@@ -0,0 +1,311 @@
+package ru.dtechnologies.schedule_train;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.DatePicker;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, AdapterView.OnItemClickListener,
+        View.OnClickListener{
+
+    private static final String LOG_TAG = "myLogs";
+    private static final String TAG_DATA = "extra_data";
+
+    private static final String TAG_LOCATION_1 = "location_1";
+    private static final String TAG_LOCATION_2 = "location_2";
+    private static final String TAG_NAME_STATION = "stationTitle";
+    private static final String TAG_IMAGE = "image";
+    final int station_d = R.drawable.icon_station_from;
+    final int station_a = R.drawable.icon_station_in;
+
+    final String SAVED_STATION_A = "saved_station_a";
+    final String SAVED_STATION_D = "saved_station_d";
+    final String SAVED_LOCATION_A1 = "saved_location_a1";
+    final String SAVED_LOCATION_A2 = "saved_location_a2";
+    final String SAVED_LOCATION_D1 = "saved_location_d1";
+    final String SAVED_LOCATION_D2 = "saved_location_d2";
+    final String SAVED_DATE = "saved_date";
+    boolean networkState = true;
+
+    DrawerLayout drawer;
+    ListView lvStation;
+    RelativeLayout layoutDate;
+
+    SimpleAdapter adapter;
+    TextView tvDate;
+
+    SharedPreferences sPref;
+
+    ArrayList<HashMap<String, Object>> listStation = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        lvStation = (ListView) findViewById(R.id.lvStations);
+        init_list(listStation, 2);
+        adapter = new SimpleAdapter(MainActivity.this, listStation, R.layout.item_schedule ,
+                new String[]{ TAG_NAME_STATION, TAG_LOCATION_1, TAG_LOCATION_2, TAG_IMAGE},
+                new int[]{R.id.tvStation, R.id.tvCountry_Region, R.id.tvSity_District, R.id.image_station});
+        lvStation.setAdapter(adapter);
+        lvStation.setOnItemClickListener(this);
+
+        tvDate = (TextView) findViewById(R.id.tvDate);
+        layoutDate = (RelativeLayout) findViewById(R.id.layout_calendar);
+        layoutDate.setOnClickListener(this);
+
+        drawer.openDrawer(Gravity.LEFT);
+    }
+
+    public void init_list(ArrayList<HashMap<String, Object>> arrayList, int length){
+        HashMap<String, Object> map = null;
+        for (int i = 0; i < length; i++) {
+            map = new HashMap<>();
+            map.put(TAG_NAME_STATION, "");
+            map.put(TAG_LOCATION_1, "");
+            map.put(TAG_LOCATION_2, "");
+            if (i == 0){
+                map.put(TAG_IMAGE, station_d);
+            }else{
+                map.put(TAG_IMAGE, station_a);
+            }
+
+            arrayList.add(i, map);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+
+        if(getIntent().hasExtra(TAG_DATA)) {
+            String str = getIntent().getStringExtra(TAG_DATA);
+            loadSetting();
+
+            if (tvDate.getText().length() == 0) {
+                SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+                tvDate.setHint(sdf.format(new Date(System.currentTimeMillis())));
+            }
+
+            if (str.equals("from")) {
+                HashMap<String, Object> map = new HashMap<>();
+                map.put(TAG_NAME_STATION, getIntent().getStringExtra(TAG_NAME_STATION));
+                map.put(TAG_LOCATION_1, getIntent().getStringExtra(TAG_LOCATION_1));
+                map.put(TAG_LOCATION_2, getIntent().getStringExtra(TAG_LOCATION_2));
+                map.put(TAG_IMAGE, station_d);
+
+                listStation.set(0, map);
+            }
+            if (str.equals("to")) {
+                HashMap<String, Object> map = new HashMap<>();
+                map.put(TAG_NAME_STATION, getIntent().getStringExtra(TAG_NAME_STATION));
+                map.put(TAG_LOCATION_1, getIntent().getStringExtra(TAG_LOCATION_1));
+                map.put(TAG_LOCATION_2, getIntent().getStringExtra(TAG_LOCATION_2));
+                map.put(TAG_IMAGE, station_a);
+
+                listStation.set(1, map);
+            }
+        }
+
+        if (isNetworkAvailable()){
+            networkState = true;
+        }else{
+            networkState = false;
+        }
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null;
+    }
+
+    @Override
+    public void onBackPressed() {
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_schedule) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (id == R.id.nav_about) {
+            // показ диалога с информацией о копирайте и версии
+            String versionName = BuildConfig.VERSION_NAME;
+            int versionCode = BuildConfig.VERSION_CODE;
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle(R.string.title_dialog)
+                    .setMessage("© Д. А. Иванов, 2016" + "\n\nВерсия " + versionName + " (сборка " + versionCode + ")")
+                    .setCancelable(false)
+                    .setNegativeButton(R.string.btn_OK,
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+        return true;
+    }
+
+    public void saveSetting(){
+        sPref = getSharedPreferences("SCHEDULE_SETTING", MODE_PRIVATE);
+        SharedPreferences.Editor edHelp = sPref.edit();
+        edHelp.putString(SAVED_STATION_D, String.valueOf(listStation.get(0).get(TAG_NAME_STATION)));
+        edHelp.putString(SAVED_STATION_A, String.valueOf(listStation.get(1).get(TAG_NAME_STATION)));
+        //
+        edHelp.putString(SAVED_LOCATION_D1, String.valueOf(listStation.get(0).get(TAG_LOCATION_1)));
+        edHelp.putString(SAVED_LOCATION_D2, String.valueOf(listStation.get(0).get(TAG_LOCATION_2)));
+        //
+        edHelp.putString(SAVED_LOCATION_A1, String.valueOf(listStation.get(1).get(TAG_LOCATION_1)));
+        edHelp.putString(SAVED_LOCATION_A2, String.valueOf(listStation.get(1).get(TAG_LOCATION_2)));
+
+        edHelp.putString(SAVED_DATE, tvDate.getText().toString());
+        edHelp.commit();
+    }
+
+    public void loadSetting(){
+        sPref = getSharedPreferences("SCHEDULE_SETTING", MODE_PRIVATE);
+
+        HashMap<String, Object> map = null;
+        // загрузка данных отправления
+        map = new HashMap<>();
+            map.put(TAG_NAME_STATION, sPref.getString(SAVED_STATION_D, ""));
+            map.put(TAG_LOCATION_1, sPref.getString(SAVED_LOCATION_D1, ""));
+            map.put(TAG_LOCATION_2, sPref.getString(SAVED_LOCATION_D2, ""));
+            map.put(TAG_IMAGE, station_d);
+
+        listStation.set(0, map);
+
+        // загрузка данных прибытия
+        map = new HashMap<>();
+            map.put(TAG_NAME_STATION, sPref.getString(SAVED_STATION_A, ""));
+            map.put(TAG_LOCATION_1, sPref.getString(SAVED_LOCATION_A1, ""));
+            map.put(TAG_LOCATION_2, sPref.getString(SAVED_LOCATION_A2, ""));
+            map.put(TAG_IMAGE, station_a);
+
+        listStation.set(1, map);
+
+        tvDate.setText(sPref.getString(SAVED_DATE, ""));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        saveSetting();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent = null;
+        switch (position){
+            case 0:
+                intent = new Intent(MainActivity.this, ChooseActivity.class);
+                intent.putExtra(TAG_DATA, "from");
+                startActivity(intent);
+                break;
+
+            case 1:
+                intent = new Intent(MainActivity.this, ChooseActivity.class);
+                intent.putExtra(TAG_DATA, "to");
+                startActivity(intent);
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.layout_calendar:
+                callDatePicker();
+                break;
+        }
+    }
+
+    private void callDatePicker() {
+        // получаем текущую дату
+        final Calendar cal = Calendar.getInstance();
+        int mYear = cal.get(Calendar.YEAR);
+        int mMonth = cal.get(Calendar.MONTH);
+        int mDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        // инициализируем диалог выбора даты текущими значениями
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        String editTextDateParam = dayOfMonth + "." + (monthOfYear + 1) + "." + year;
+                        tvDate.setText(editTextDateParam);
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+}
